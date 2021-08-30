ac7c0a8c1140ab91fcc1587080480612750ef8dd
Michael Rodham <switchytm@gmail.com>
2021-08-30 15:17:34 +0100

    More evidence

--- src/App.vue
+++ src/App.vue
@@ -57,10 +57,27 @@ const actions = {
   breathing: "Disembodied Breathing",
   hissing: "Hissing",
   breakerOff: "Turns off breaker",
-  toggleBreaker: "Turn On/Off breaker",
+  toggleBreaker: "Turn on/off breaker",
   openDrawers: "Opens drawers",
   liftsAllObjects: "Lifts all objects",
-  appear: "Appears"
+  appear: "Appears",
+  chairCeiling: "Pins chair to ceiling",
+  toggleLights: "Turns on/off lights",
+  toggleDoors: "Opens/closes doors",
+  toggleOven: "Turns on/off oven",
+  toggleTV: "Turns on/off television",
+  toggleRadio: "Turns on/off radio",
+  toggleKettle: "Turns on/off kettle",
+  kettleOn: "Turns on kettle",
+  knockPaintings: "Knocks paintings off of walls",
+  upsideDownPainting: "Paintings upside down.",
+  openDoors: "Opens doors",
+  scribblePaintings: "Scribbles on paintings",
+  stealsRugs: "Steals rugs",
+  stealPaintings: "Steals paintings",
+  bloodPools: "Blood pools",
+  hangDoll: "Hangs voodoo doll from ceiling",
+  
 }
 
 export default {
@@ -85,28 +102,28 @@ export default {
       actions: actions,
       ghosts: {
         effigy: {
-          actions: [actions.throwKnives, actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.stringUkelele, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.changeFlashlight, actions.graves, actions.crucifixWalls, actions.lockDoors, actions.blowsOutCandles, actions.toggleBreaker]
+          actions: [actions.throwKnives, actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.stringUkelele, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.changeFlashlight, actions.graves, actions.crucifixWalls, actions.lockDoors, actions.blowsOutCandles, actions.toggleBreaker, actions.chairCeiling, actions.toggleLights, actions.toggleDoors, actions.toggleTV, actions.toggleRadio, actions.toggleKettle, actions.knockPaintings]
         },
         rusalka: {
-          actions: [actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.writingMirrors, actions.stringUkelele, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.changeFlashlight, actions.lockDoors, actions.blowsOutCandles, actions.breathing, actions.breakerOff, actions.liftsAllObjects]
+          actions: [actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.writingMirrors, actions.stringUkelele, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.changeFlashlight, actions.lockDoors, actions.blowsOutCandles, actions.breathing, actions.breakerOff, actions.liftsAllObjects, actions.chairCeiling, actions.toggleDoors, actions.toggleOven, actions.toggleTV, actions.toggleRadio, actions.toggleKettle, actions.knockPaintings]
         },
         demon: {
-          actions: [actions.throwKnives, actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.changeFlashlight, actions.graves, actions.crucifixWalls, actions.crucifixOutside, actions.pentagrams, actions.lockDoors, actions.hissing, actions.toggleBreaker]
+          actions: [actions.throwKnives, actions.stabWalls, actions.floatingPainting, actions.stealItems, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.changeFlashlight, actions.graves, actions.crucifixWalls, actions.crucifixOutside, actions.pentagrams, actions.lockDoors, actions.hissing, actions.toggleBreaker, actions.chairCeiling, actions.toggleLights, actions.toggleOven, actions.toggleTV, actions.toggleRadio, actions.knockPaintings, actions.openDoors, actions.scribblePaintings]
         },
         shade: {
-          actions: [actions.writingMirrors, actions.stringUkelele, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.blowsOutCandles, actions.breathing, actions.opensDrawers]
+          actions: [actions.writingMirrors, actions.stringUkelele, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.blowsOutCandles, actions.breathing, actions.opensDrawers, actions.toggleLights, actions.toggleDoors, actions.toggleOven, actions.toggleTV, actions.toggleRadio, actions.toggleKettle, actions.knockPaintings]
         },
         oni: {
-          actions: [actions.throwKnives, actions.rainBlood, actions.floatingPainting, actions.bloodWriting, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.crucifixOutside, actions.lockDoors, actions.blowsOutCandles, actions.toggleBreaker]
+          actions: [actions.throwKnives, actions.rainBlood, actions.floatingPainting, actions.bloodWriting, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.crucifixOutside, actions.lockDoors, actions.blowsOutCandles, actions.toggleBreaker, actions.chairCeiling, actions.toggleLights, actions.toggleOven, actions.toggleTV, actions.toggleRadio, actions.toggleKettle, actions.openDoors, actions.scribblePaintings]
         },
         yurei: {
-          actions: [actions.throwKnives, actions.flies, actions.sinkBlood, actions.floatingPainting, actions.writingMirrors, actions.bloodWriting, actions.bloodHandprints, actions.smashPlates, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.graves, actions.crucifixOutside, actions.knocksDownFridge, actions.blowsOutCandles, actions.breathing, actions.opensDrawers, actions.liftsAllObjects]
+          actions: [actions.throwKnives, actions.flies, actions.sinkBlood, actions.floatingPainting, actions.writingMirrors, actions.bloodWriting, actions.bloodHandprints, actions.smashPlates, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.graves, actions.crucifixOutside, actions.knocksDownFridge, actions.blowsOutCandles, actions.breathing, actions.opensDrawers, actions.liftsAllObjects, actions.toggleLights, actions.kettleOn]
         },
         mare: {
-          actions: [actions.moths, actions.cockroaches, actions.bathBlood, actions.sinkBlood, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.changeFlashlight, actions.crucifixWalls, actions.knocksDownFridge, actions.blowsOutCandles, actions.appear]
+          actions: [actions.moths, actions.cockroaches, actions.bathBlood, actions.sinkBlood, actions.stringUkelele, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.emptyBookcase, actions.killPlants, actions.changeFlashlight, actions.crucifixWalls, actions.knocksDownFridge, actions.blowsOutCandles, actions.appear, actions.toggleTV, actions.toggleKettle, actions.knockPaintings, actions.upsideDownPainting, actions.stealsRugs, actions.stealPaintings, actions.bloodPools, actions.hangDoll]
         },
         chimera: {
-          actions: [actions.stabWalls, actions.flies, actions.moths, actions.cockroaches, actions.sinkBlood, actions.bloodWriting, actions.bloodHandprints, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.disableFlashlight, actions.fakeHunt, actions.pentagrams, actions.knocksDownFridge, actions.duplicateItems, actions.toggleBreaker]
+          actions: [actions.stabWalls, actions.flies, actions.moths, actions.cockroaches, actions.sinkBlood, actions.bloodWriting, actions.bloodHandprints, actions.smashPlates, actions.smashBottles, actions.rockingChair, actions.ringPhone, actions.disableFlashlight, actions.fakeHunt, actions.pentagrams, actions.knocksDownFridge, actions.duplicateItems, actions.toggleBreaker, actions.toggleLights, actions.toggleDoors, actions.toggleOven, actions.toggleTV, actions.toggleKettle]
         }
       }
     }
