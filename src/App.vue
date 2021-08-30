a01f84e2aa42e82118cc1899cf61ad521f53aff2
Michael Rodham <switchytm@gmail.com>
2021-08-30 16:45:51 +0100

    oni knocks paintings

--- src/App.vue
+++ src/App.vue
@@ -294,6 +294,7 @@ export default {
             actions.bruises,
             actions.movesKidsToys,
             actions.throwsKidsBall,
+            actions.knockPaintings,
           ],
         },
         yurei: {
